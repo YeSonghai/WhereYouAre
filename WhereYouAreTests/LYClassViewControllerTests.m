e682456779d855d22e9e0d0233da0907bfc01750
YeSonghai <sean.yeph@gmail.com>
2013-01-06 18:02:23 +0800

    test

--- WhereYouAreTests/LYClassViewControllerTests.m
+++ WhereYouAreTests/LYClassViewControllerTests.m
@@ -38,7 +38,7 @@
 - (void) testInitMainTopbar
 {
     [self.mMainVC initMainTopbar];
-    STAssertNotNil(self.mMainVC.mainTopBarView, @"mainTopBarView Init fall");
+    STAssertNil(self.mMainVC.mainTopBarView, @"mainTopBarView Init fall");
 }
 
 
